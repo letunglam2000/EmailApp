04be83aa9370d02f1b5c6df2016acb06fe75fcd6
lam <letunglam2000@gmail.com>
2024-02-17 18:08:41 +0100

    Adds set mailBoxCapacity, set AlternateEmail and changePassword methods.

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -8,9 +8,10 @@ public class Email {
     private String lastName;
     private String password;
     private String department;
+    private String email;
     private int mailboxCapacity;
     private String alternateEmail;
-    private String company;
+    private String companySuffix = "aeycompany.com";
 
     // Constructor to receive the first name and last name
     public Email(String firstName, String lastName) {
@@ -21,6 +22,17 @@ public class Email {
         // Call a method asking for the department - return the department
         this.department = setDepartment();
         System.out.println("Department: " + this.department);
+
+        // Call a method that returns a random password
+        this.password = randomPassword(8);
+        System.out.println("Your password is: " + this.password);
+
+        // Combine elements to generate email
+        email = firstName.toLowerCase() + "."+ lastName.toLowerCase() + "@" + department + "." + companySuffix;
+        System.out.println("Your email is: " + email);
+
+
+
     }
 
     // Ask for the department
@@ -41,15 +53,28 @@ public class Email {
     }
 
     // Generate a random password
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+        char[] password = new char[length];
+        for (int i=0; i<length; i++) {
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
 
     // Set the mailbox capacity
+    public void setMailboxCapacity(int capacity) {
+        this.mailboxCapacity = capacity;
+    }
 
     // Set the alternate email
+    public void setAlternateEmail(String alternateEmail) {
+        this.alternateEmail = alternateEmail;
+    }
 
-    // Change the email
-
-    @Override
-    public String toString() {
-        return firstName + "." + lastName + "@" + department + "." + company + ".com";
+    // Change the password
+    public void changePassword (String password) {
+        this.password = password;
     }
 }
