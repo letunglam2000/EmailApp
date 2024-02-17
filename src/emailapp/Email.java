e61c301b113895d999ba8c07d080246e28bed059
lam <letunglam2000@gmail.com>
2024-02-17 17:27:49 +0100

    Adds the setDepartment method.

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
 
     private String firstName;
@@ -10,15 +12,30 @@ public class Email {
     private String alternateEmail;
     private String company;
 
-
-
     // Constructor to receive the first name and last name
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+
+        // Call a method asking for the department - return the department
+        this.department = setDepartment();
     }
 
     // Ask for the department
+    private String setDepartment() {
+        System.out.println("""
+                Enter the department
+                1 for Sales
+                2 for Development
+                3 for Accounting
+                0 for none""");
+        Scanner in = new Scanner(System.in);
+        int deptChoice = in.nextInt();
+        if (deptChoice == 1) { return "sales"; }
+        else if (deptChoice == 2) { return "dev"; }
+        else if (deptChoice == 3) { return "acct"; }
+        else { return ""; }
+    }
 
     // Generate a random password
 
